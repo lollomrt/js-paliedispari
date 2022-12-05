30b83cd5a33ece461e46fcace2bb6edc4d38c80d
lollomrt <lorenzomartini.mrt@outlook.it>
2022-12-05 20:57:26 +0100

    Fine bonus

--- Pari e Dispari/js/javascript.js
+++ Pari e Dispari/js/javascript.js
@@ -13,34 +13,40 @@ function controlloSomma(num) {
         return "dispari"
     }
 }
-//Dare all'utente la possibilità di scegliere tra pari o dispari.
-let scelta = prompt("Scegli tra pari o dispari scrivendo la parola intera es: pari")
-console.log(scelta)
+let scelta
+let numeroUser
 
-//Dare all'utente la possibilità di scegliere un numero da 1 a 5.
-let numeroUser = parseInt(prompt("Scegli un numero da 1 a 5"))
-console.log(numeroUser)
+const cta = document.getElementById("cta")
+cta.addEventListener ("click", function(){
+    //Dare all'utente la possibilità di scegliere tra pari o dispari.
+    scelta = document.getElementById("seleziona-pari-dispari").value 
+    console.log(scelta)
+    
+    //Dare all'utente la possibilità di scegliere un numero da 1 a 5.
+    numeroUser = parseInt(document.getElementById("numero").value)
+    console.log(numeroUser)
 
-// Richiamo la funzione per la generazione di un numero intero compreso tra 1 e 5
-let numeroComputer = numeroRandom(1,5)
-console.log(numeroComputer)
+    // Richiamo la funzione per la generazione di un numero intero compreso tra 1 e 5
+    let numeroComputer = numeroRandom(1,5)
+    console.log(numeroComputer)
 
-//Creao una variabile somma che mi vada a sommare il numero inserito dall'utente ed il numero random generato
-let somma = parseInt(numeroUser + numeroComputer)
-console.log(somma)
+    //Creao una variabile somma che mi vada a sommare il numero inserito dall'utente ed il numero random generato
+    let somma = parseInt(numeroUser + numeroComputer)
+    console.log(somma)
 
-//Richiamo la funzione all'esterno ed assegno il suo valore alla variabile pariOdispari
-let pariOdispari = controlloSomma(somma)
-console.log(pariOdispari)
+    //Richiamo la funzione all'esterno ed assegno il suo valore alla variabile pariOdispari
+    let pariOdispari = controlloSomma(somma)
+    console.log(pariOdispari)
 
-//Controllo se l'utente ha vinto:
-//SE ha inserito pari ed esce una somma pari --> Hai vinto!
-if (scelta == "pari" && pariOdispari == "pari"){
-    alert("HAI VINTO - LA SOMMA È PARI!")
-} //SE ha inserito dispari ed esce una somma dispari --> Hai vinto!
-else if (scelta == "dispari" && pariOdispari == "dispari"){
-    alert("HAI VINTO - LA SOMMA È DISPARI!")
-} //ALTRIMENTI --> Hai perso! 
-else {
-    alert("HAI PERSO :(")
-}
+    //Controllo se l'utente ha vinto:
+    //SE ha inserito pari ed esce una somma pari --> Hai vinto!
+    if (scelta == "pari" && pariOdispari == "pari"){
+        alert("HAI VINTO - LA SOMMA È PARI!")
+    } //SE ha inserito dispari ed esce una somma dispari --> Hai vinto!
+    else if (scelta == "dispari" && pariOdispari == "dispari"){
+        alert("HAI VINTO - LA SOMMA È DISPARI!")
+    } //ALTRIMENTI --> Hai perso! 
+    else {
+        alert("HAI PERSO :(")
+    }
+})
